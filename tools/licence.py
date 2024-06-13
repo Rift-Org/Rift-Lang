f0e605e038562cec47b3e25e99e1af2f4c189181
Amanuel Bogale <a_bogale@apple.com>
2024-06-13 15:37:54 -0700

    adding licence

--- tools/licence.py
+++ tools/licence.py
@@ -1,4 +1,7 @@
-/////////////////////////////////////////////////////////////
+import os
+
+# Define the string to be appended
+header = """/////////////////////////////////////////////////////////////
 ///                                                       ///
 ///     ██████╗ ██╗███████╗████████╗                      ///
 ///     ██╔══██╗██║██╔════╝╚══██╔══╝                      ///
@@ -12,11 +15,26 @@
 ///                                                       ///
 /////////////////////////////////////////////////////////////
 
-#include <reader/reader.hh>
+"""
 
-namespace rift
-{
-    namespace reader
-    {
-    }
-}
+
+directory = "../lib/"
+
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        file_path = os.path.join(root, file)
+        
+        try:
+            
+            with open(file_path, "r", encoding="utf-8", errors="replace") as f:
+                content = f.read()
+            
+            
+            modified_content = header + content
+            
+            
+            with open(file_path, "w", encoding="utf-8") as f:
+                f.write(modified_content)
+        except UnicodeDecodeError as e:
+            print(f"Skipping file '{file_path}' due to decoding error: {str(e)}")
